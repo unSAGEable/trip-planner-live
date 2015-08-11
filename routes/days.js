3abe6789309a849310bfe0794d2f79e035b7c173
Kevin Hoang <find.kevinhoang@gmail.com>
2015-08-10 23:06:52 -0400

    typo!! on days.js

--- routes/days.js
+++ routes/days.js
@@ -5,7 +5,7 @@ var models ={}
 models.Hotel = mongoose.model('Hotel')
 models.ThingToDo = mongoose.model('ThingToDo')
 models.Restaurant = mongoose.model('Restaurant')
-models.Restaurant = mongoose.model('Day')
+models.Day = mongoose.model('Day')
 
 // serves up all days
 dayRouter.get('/',
