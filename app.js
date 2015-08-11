f187e0104a42c9c1416e91c3cea462f2069de34d
Kevin Hoang <find.kevinhoang@gmail.com>
2015-08-10 20:27:00 -0400

    app change

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use(function(err, req, res, next) {
 
 // listen on a port
 var port = 3000;
-app.listen(port, function () {
-	console.log('The server is listening closely on port', port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
